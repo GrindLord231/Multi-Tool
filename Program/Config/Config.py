c3d3a25b38e546b4bdf5ed696ab482ae6047fd24
GrindLord231 <s86397233@gmail.com>
2025-03-25 13:12:10 -0500

    Add files via upload

---
+++ Program/Config/Config.py
@@ -0,0 +1,24 @@
+# Copyright (c) RedTiger (https://redtiger.shop)
+# See the file 'LICENSE' for copying permission
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+name_tool       =  "RedTiger"
+type_tool       =  "Multi-Tools"
+version_tool    =  "6.3"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "GrindLord231"
+platform        =  "Windows 10/11 & Linux"
+website         =  "GrindLord.shop"
+github_tool     =  "https://github.com/GrindLord231"
+telegram        =  "t.me/redtigersupport"
+url_config      =  "https://github.com/GrindLord231"
+url_downloads   =  "https://github.com/GrindLord231"
+license         =  "https://github.com/GrindLord231"
+copyright       =  "Copyright (c) RedTiger 'GPL-3.0 license'"
